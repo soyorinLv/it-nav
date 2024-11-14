3122021b298432469d22b726cc9d1b0af38aff2f
soyorinLv <158822153+soyorinLv@users.noreply.github.com>
2024-11-14 11:47:03 +0800

    完善 vite.config配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { visualizer } from 'rollup-plugin-visualizer';
 
-
 export default defineConfig({
-  base: "/it-nav/",  // 替换为你的仓库名称或部署子路径
+  base: "/it-nav/",  // 将此处替换为你的仓库名称或部署子路径
   server: {
     host: "0.0.0.0",
     port: 5173,
@@ -16,4 +16,3 @@ export default defineConfig({
     },
   },
 });
-
